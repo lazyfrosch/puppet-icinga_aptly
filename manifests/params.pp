87de0336f2ce50403a011e0851c33897278bcfcd
Markus Frosch <markus.frosch@icinga.com>
2018-12-07 11:33:05 +0100

    params: Fix gpg package selection

--- manifests/params.pp
+++ manifests/params.pp
@@ -7,8 +7,8 @@ class icinga_aptly::params {
   $_release = $facts['os']['release']['major']
 
   if (
-    ($_dist == 'Debian' and versioncmp($_release, '9') < 1)
-    or ($_dist == 'Ubuntu' and versioncmp($_release, '18.04') < 1)
+    ($_dist == 'Debian' and versioncmp($_release, '9') < 0)
+    or ($_dist == 'Ubuntu' and versioncmp($_release, '18.04') < 0)
   ) {
     $gpg_packages = ['gnupg', 'gpgv']
   } else {
